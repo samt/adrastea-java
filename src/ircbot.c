b395fb511921202bc1521f2facbb9cb5c6408fad
Sam Thompson <sam@websyntax.net>
2011-02-14 15:58:46 -0800

    Added some initial files
    docs/ and tests/ were added at this point.

---
+++ src/ircbot.c
@@ -0,0 +1,20 @@
+/**
+ *
+ * Adrastea IRC Bot v2
+ * ircbot.c - has main()
+ *
+ * Copyright (c) 2011 Sam Thompson <sam@websyntax.net>
+ * The MIT License http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
+
