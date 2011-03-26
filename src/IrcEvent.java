60a264bc5e4732c3ba3cc21d4786d02c06eb890a
Sam Thompson <sam@websyntax.net>
2011-03-26 10:23:52 -0700

    Initial files...

---
+++ src/IrcEvent.java
@@ -0,0 +1,10 @@
+/*
+ * Adrastea IRC Bot
+ * Version 2.0.0 (Java)
+ *
+ * (c) 2011 Sam Thompson <sam@websyntax.net>
+ * License: http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+public class IrcEvent {
+}
